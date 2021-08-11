ec5d9265f6648ab58a7c599c804c3f58123b1624
Chetas Pahuja <75940206+chetaspahuja@users.noreply.github.com>
2021-08-11 17:16:13 +0530

    Update Recommendation.py

--- Recommendation.py
+++ Recommendation.py
@@ -17,23 +17,21 @@ ratings_df
 
 ratings_df.shape
 
-
-popular_products = pd.DataFrame(ratings_df.groupby('ProductId')['Rating'].count())
 # Grouping data according to number of feedback/rating in the dataset
-most_popular = popular_products.sort_values('Rating', ascending=False)
-# Sorting the product list with most rated product on top
+popular_products = pd.DataFrame(ratings_df.groupby('ProductId')['Rating'].count())
 
-print(popular_products)
-print(most_popular)
+# Sorting the product list with most rated product on top
+most_popular = popular_products.sort_values('Rating', ascending=False)
+
+# print(popular_products)
+# print(most_popular)
 
 # taking top 10 most rated products
 most_popular.head(10)
 
 # plotting for top 30 most rated products
-most_popular.head(30).plot(kind="bar")
 
-# fig = plt.bar(ratings_df , x = "most_popular" , title = 'Number of Ratings')
-# fig.show() 
+most_popular.head(30).plot(kind="bar")
 
 
 # THE ABOVE HELPS TO RECOMMEND THE PRODUCT TO A NEW USER WITH NO VIEW HISTORY
@@ -45,7 +43,7 @@ most_popular.head(30).plot(kind="bar")
 ratings_1 = ratings_df.head(10000)
 ratings_df_utility_matrix = ratings_1.pivot_table(
     values='Rating', index='UserId', columns='ProductId', fill_value=0)
-print(ratings_df_utility_matrix.head())
+# print(ratings_df_utility_matrix.head())
 
 ratings_df_utility_matrix.shape
 
@@ -61,22 +59,21 @@ decomposed_matrix = SVD.fit_transform(trans)
 decomposed_matrix.shape
 
 correlation_matrix = np.corrcoef(decomposed_matrix)
-# correlation_matrix.shape
-print(correlation_matrix)
+correlation_matrix.shape
+# print(correlation_matrix)
 
-# ___________________________________________________________________________________
 # ISOLATING A PRODUCT ID
 
-i = trans.index[7]
+i = trans.index[9]
 
 product_name = list(trans.index)
 product_ID = product_name.index(i)
 product_ID
 
 correlation_product_ID = correlation_matrix[product_ID]
-print(correlation_product_ID > 0.9)
+# print(correlation_product_ID > 0.9)
 
-correlation_product_ID.shape
+# correlation_product_ID.shape
 
 Recommend = list(trans.index[correlation_product_ID > 0.90])
 
@@ -84,3 +81,37 @@ Recommend = list(trans.index[correlation_product_ID > 0.90])
 Recommend.remove(i)
 print("LIST")
 print(Recommend[0:9]) 
+
+# -------------------------------------------------------------------------------
+
+# Importing libraries 
+from sklearn.feature_extraction.text import TfidfVectorizer , CountVectorizer 
+from sklearn.neighbors import NearestNeighbors 
+from sklearn.cluster import KMeans 
+from sklearn.metrics import adjusted_rand_score 
+
+# RECOMMENDING PRODUCTS BASED ON DESCRIPTION 
+product_description = pd.read_csv('C:\GIT\Datasets\product_descriptions.csv')
+# product_description.info() 
+product_description.shape
+
+#Checking for empty values , dropna -> removes missing values 
+product_description = product_description.dropna() 
+product_description.shape 
+
+product_description.head() #By defualt head(5) -> top 5 most values 
+
+#  Converting the text into numerical for data analysis 
+vectorizer = TfidfVectorizer(stop_words = 'english') 
+X1 = vectorizer.fit_transform(product_description["product_description"]) 
+# print(X1) 
+
+
+X=X1
+
+kmeans = KMeans(n_clusters = 10, init = 'k-means++')
+y_kmeans = kmeans.fit_predict(X)
+plt.plot(y_kmeans, ".")
+plt.show()
+
+# Reduce the dataset , takes long time to process 
